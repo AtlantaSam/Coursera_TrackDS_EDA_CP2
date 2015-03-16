22f286f3c5c79bafbc5437a2351aa9c7bcd30094
AtlantaSam <edge33@mac.com>
2015-03-16 08:17:32 -0400

    version 1

---
+++ plot1.R
@@ -0,0 +1,9 @@
+## plot1.R
+
+## Set working directory
+setwd("~/Documents/data/coursera/EDA_CP2")
+
+## Read in the data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
